b8e419b3ded48d0099b7b4a4ed49d520bd4e8164
iantoon0 <ihunickesmith@gmail.com>
2017-11-11 13:21:37 -0700

    Initial arduino code commit

---
+++ ABox_CUno_Code/ABox_CUno_Code.ino
@@ -0,0 +1,13 @@
+int iVoltageOutput;
+float fRotorCurrent, fVoltageOutput;
+int iLCDOutputType; //1: rotor current 2: Stator voltage 3: Generated Power
+const int rotorPin = 0, currentMeasurePin=1, statorVoltagePin = 2;
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
