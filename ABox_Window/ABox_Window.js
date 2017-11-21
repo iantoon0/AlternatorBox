d15f3688d8c943a0ed895407d74d2de45e784cd7
iantoon0 <ihunickesmith@gmail.com>
2017-11-21 11:17:17 -0700

    Began working on NodeJS implementation

---
+++ ABox_Window/ABox_Window.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res){
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    fs.readFile('window.html', function(err, data){
+        res.write('Hello world!');
+        res.end();
+    })
+}).listen(8080);
