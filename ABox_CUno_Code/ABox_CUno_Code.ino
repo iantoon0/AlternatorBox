582d3dd838393d7b3d8ab4da2c612db34a10791b
iantoon0 <ihunickesmith@gmail.com>
2017-11-21 15:01:06 -0700

    Continued work on ABox Window and CUno Code

--- ABox_CUno_Code/ABox_CUno_Code.ino
+++ ABox_CUno_Code/ABox_CUno_Code.ino
@@ -1,13 +1,23 @@
+#include <ArduinoJson.h>
+
 int iVoltageOutput;
 float fRotorCurrent, fVoltageOutput;
 int iLCDOutputType; //1: rotor current 2: Stator voltage 3: Generated Power
-const int rotorPin = 0, currentMeasurePin=1, statorVoltagePin = 2;
+int dRotorOutputPin = 2, aCurrentMeasurePin=2, aStatorVoltagePinArray[] = {1,2,3,4}, aBatteryPin = 5;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(28800);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
+
+class BoxState
+{
+  float fRotorCurrent, fRotorVoltage, fStatorLoopVoltageArray[];
+  bool bBeamBroken;
+};
+
--- ABox_Window/ABox_Window.js
+++ ABox_Window/ABox_Window.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var fs = require('fs');
+
 http.createServer(function (req, res){
     res.writeHead(200,{'Content-Type': 'text/html'});
     fs.readFile('window.html', function(err, data){
-        res.write('Hello world!');
+        res.write(data);
         res.end();
     })
 }).listen(8080);
