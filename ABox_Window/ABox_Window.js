967e0d2d2c453bb4967da4ae894217a87a06bb34
iantoon0 <ihunickesmith@gmail.com>
2017-11-21 20:27:03 -0700

    Began adding raspberry pi implementation code

--- ABox_Window/ABox_Window.js
+++ ABox_Window/ABox_Window.js
@@ -1,6 +1,15 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http'),
+fs = require('fs'),
+raspi = require('raspi'),
+Serial = require('raspi-serial').Serial;
 
+var port = new Serial({'baudRate':28800}),
+dataRecieved = function(data){
+    JSON.parse(data);
+};
+
+
+port.on('data', dataRecieved(data));
 http.createServer(function (req, res){
     res.writeHead(200,{'Content-Type': 'text/html'});
     fs.readFile('window.html', function(err, data){
