5755f52bc69c95346deb0894cdc15319798bd413
iantoon0 <ihunickesmith@gmail.com>
2017-11-23 00:18:28 -0700

    Added currentState object reference, figured out what in the world C++ even is

--- ABox_CUno_Code/ABox_CUno_Code.ino
+++ ABox_CUno_Code/ABox_CUno_Code.ino
@@ -6,7 +6,7 @@ class BoxState
 {
   float fRotorCurrent, fRotorVoltage, fStatorLoopVoltageArray[];
   bool bBeamBroken;
-  BoxState(float fRotorCurrentVar, float fRotorVoltageVar, float fStatorLoopVoltageArrayVar[], bool bBeamBrokenVar)
+  public: BoxState(float fRotorCurrentVar, float fRotorVoltageVar, float fStatorLoopVoltageArrayVar[], bool bBeamBrokenVar)
   {
     fRotorCurrent = fRotorCurrentVar;
     fRotorVoltage = fRotorVoltageVar;
@@ -19,6 +19,8 @@ int iVoltageOutput;
 float fRotorCurrent, fVoltageOutput;
 int iLCDOutputType; //1: rotor current 2: Stator voltage 3: Generated Power
 int dRotorOutputPin = 2, aCurrentMeasurePin=2, aStatorVoltagePinArray[] = {1,2,3,4}, aBatteryPin = 5;
+BoxState* currentState;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(28800);
@@ -26,7 +28,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // BoxState() currentState = new BoxState();
+  float testArray[] = {0.0f, 0.0f};
+  currentState = new BoxState(0.0f, 0.0f, testArray, true);
 }
 
 
