b048459611c3bea8f295f5a1a39f2e3d59f59567
iantoon0 <ihunickesmith@gmail.com>
2017-11-27 10:58:08 -0700

    Finished making objects work

--- ABox_CUno_Code/ABox_CUno_Code.ino
+++ ABox_CUno_Code/ABox_CUno_Code.ino
@@ -1,3 +1,5 @@
+#include <QueueArray.h>
+
 #include <ArduinoJson.h>
 #define arr_len( x )  ( sizeof( x ) / sizeof( *x ) )
 
@@ -22,10 +24,11 @@ int iVoltageOutput;
 float fRotorCurrent, fVoltageOutput;
 int iLCDOutputType; //1: rotor current 2: Stator voltage 3: Generated Power
 int dRotorOutputPin = 2, aCurrentMeasurePin=2, aStatorVoltagePinArray[] = {1,2,3,4}, aBatteryPin = 5;
-BoxState* currentState;
-BoxState statesList[100];
+BoxState currentState;
+QueueArray<BoxState> statesList;
 unsigned long startTime;
 byte timeBuffer[4];
+bool bIsTurning;
 
 void setup() {
   // put your setup code here, to run once:
@@ -42,13 +45,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   float testArray[] = {0.0f, 0.0f};
-  currentState = new BoxState(0.0f, 0.0f, testArray, true);
-  //addStateToList(currentState);
+  currentState = *(new BoxState(0.0f, 0.0f, testArray, true));
+  addStateToList(currentState);
 }
 
-void addStateToList(BoxState){
-  if(arr_len(statesList)>=100){
-    
+void addStateToList(BoxState state){
+  if(statesList.count()>=100){
+    statesList.pop();
+    statesList.push(state);
   }
 }
 
