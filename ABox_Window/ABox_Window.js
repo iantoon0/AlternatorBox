db3933c86e90940bd3515c50b04fae619922d6d4
iantoon0 <ihunickesmith@gmail.com>
2017-11-28 15:38:33 -0700

    Cleaned up js file

--- ABox_Window/ABox_Window.js
+++ ABox_Window/ABox_Window.js
@@ -5,15 +5,22 @@ Serial = require('raspi-serial').Serial;
 
 var port = new Serial({'baudRate':28800}),
 currentState, stateList,
+
+//Function to call when we recieve data from the CUno
 dataRecieved = function(data){
     if(data === "BEGIN"){
         port.write(Date.now());
     }
-    JSON.parse(data);
+    else{
+        
+    }
 };
+
+
 port.open()
-stateList = 
+
 port.on('data', dataRecieved(data));
+
 http.createServer(function (req, res){
     res.writeHead(200,{'Content-Type': 'text/html'});
     /*fs.readFile('window.html', function(err, data){
@@ -21,4 +28,5 @@ http.createServer(function (req, res){
         res.end();
     })*/
     res.write()
+
 }).listen(8080);
