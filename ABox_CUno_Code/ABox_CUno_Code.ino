438285a7cfa0582d83bf00db83563c93c0bd653b
iantoon0 <ihunickesmith@gmail.com>
2017-11-28 15:33:17 -0700

    Continued work on timing & objects

--- ABox_CUno_Code/ABox_CUno_Code.ino
+++ ABox_CUno_Code/ABox_CUno_Code.ino
@@ -5,9 +5,9 @@
 
 class BoxState //Holds the various measured components of the box and a time value synced with 
 {
-  float fRotorCurrent, fRotorVoltage, fStatorLoopVoltageArray[];
-  bool bBeamBroken;
-  unsigned long currentTime;
+  public: float fRotorCurrent, fRotorVoltage, fStatorLoopVoltageArray[];
+  public: bool bBeamBroken;
+  public: unsigned long currentTime;
   public: BoxState(){
     
   };
@@ -24,11 +24,12 @@ int iVoltageOutput;
 float fRotorCurrent, fVoltageOutput;
 int iLCDOutputType; //1: rotor current 2: Stator voltage 3: Generated Power
 int dRotorOutputPin = 2, aCurrentMeasurePin=2, aStatorVoltagePinArray[] = {1,2,3,4}, aBatteryPin = 5;
-BoxState currentState;
+BoxState currentState, prevState;
 QueueArray<BoxState> statesList;
 unsigned long startTime;
 byte timeBuffer[4];
 bool bIsTurning;
+int iTurningCheckTimer;
 
 void setup() {
   // put your setup code here, to run once:
@@ -46,12 +47,27 @@ void loop() {
   // put your main code here, to run repeatedly:
   float testArray[] = {0.0f, 0.0f};
   currentState = *(new BoxState(0.0f, 0.0f, testArray, true));
+
+  //Determine if the rotor is spinning
+  if(!bIsTurning){
+    bIsTurning = (currentState.bBeamBroken != prevState.bBeamBroken);
+  }
+  else if (millis() - iTurningCheckTimer >= 1000)
+  {
+    bIsTurning = (currentState.bBeamBroken != prevState.bBeamBroken);
+  }
+  if(bIsTurning && iTurningCheckTimer < millis()){
+    iTurningCheckTimer = millis();
+  }
   addStateToList(currentState);
 }
 
 void addStateToList(BoxState state){
   if(statesList.count()>=100){
     statesList.pop();
+    statesList.push(state);
+  }
+  else{
     statesList.push(state);
   }
 }
--- ABox_Window/ABox_Window.js
+++ ABox_Window/ABox_Window.js
@@ -4,16 +4,21 @@ raspi = require('raspi'),
 Serial = require('raspi-serial').Serial;
 
 var port = new Serial({'baudRate':28800}),
+currentState, stateList,
 dataRecieved = function(data){
+    if(data === "BEGIN"){
+        port.write(Date.now());
+    }
     JSON.parse(data);
 };
-
-
+port.open()
+stateList = 
 port.on('data', dataRecieved(data));
 http.createServer(function (req, res){
     res.writeHead(200,{'Content-Type': 'text/html'});
-    fs.readFile('window.html', function(err, data){
+    /*fs.readFile('window.html', function(err, data){
         res.write(data);
         res.end();
-    })
+    })*/
+    res.write()
 }).listen(8080);
