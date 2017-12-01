a30d22c6750d8034c41747e09959875e2bf802fb
iantoon0 <ihunickesmith@gmail.com>
2017-12-01 09:52:34 -0700

    Added time variable to BoxState constructor

--- ABox_CUno_Code/ABox_CUno_Code.ino
+++ ABox_CUno_Code/ABox_CUno_Code.ino
@@ -11,19 +11,20 @@ class BoxState //Holds the various measured components of the box and a time val
   public: BoxState(){
     
   };
-  public: BoxState(float fRotorCurrentVar, float fRotorVoltageVar, float fStatorLoopVoltageArrayVar[], bool bBeamBrokenVar)
+  public: BoxState(float fRotorCurrentVar, float fRotorVoltageVar, float fStatorLoopVoltageArrayVar[], bool bBeamBrokenVar, long currentTimeVar)
   {
     fRotorCurrent = fRotorCurrentVar;
     fRotorVoltage = fRotorVoltageVar;
     memcpy(fStatorLoopVoltageArray,fStatorLoopVoltageArrayVar, arr_len(fStatorLoopVoltageArrayVar));
     bBeamBroken = bBeamBrokenVar;
+    currentTime = currentTimeVar;
   }
 };
 
 int iVoltageOutput;
 float fRotorCurrent, fVoltageOutput;
 int iLCDOutputType; //1: rotor current 2: Stator voltage 3: Generated Power
-int dRotorOutputPin = 2, aCurrentMeasurePin=2, aStatorVoltagePinArray[] = {1,2,3,4}, aBatteryPin = 5;
+int dRotorOutputPin = 2, aCurrentMeasurePin=1, aStatorVoltagePinArray[] = {1,2,3,4}, aBatteryPin = 5;
 BoxState currentState, prevState;
 QueueArray<BoxState> statesList;
 unsigned long startTime;
@@ -46,7 +47,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   float testArray[] = {0.0f, 0.0f};
-  currentState = *(new BoxState(0.0f, 0.0f, testArray, true));
+  currentState = *(new BoxState(0.0f, 0.0f, testArray, true, startTime + millis()));
 
   //Determine if the rotor is spinning
   if(!bIsTurning){
