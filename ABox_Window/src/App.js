041f00b687b06f5837e59a01d756c98c3f66aad3
iantoon0 <ihunickesmith@gmail.com>
2017-12-01 08:37:46 -0700

    Began adding input elements

--- ABox_Window/src/App.js
+++ ABox_Window/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {Checkbox} from './Checkbox';
 import {LineChart} from 'react-easy-chart';
 import logo from './logo.svg';
 import './App.css';
@@ -9,28 +8,62 @@ const fs = require('fs');//,
 //Serial = require('raspi-serial').Serial;
 
 var //port = new Serial({'baudRate':28800}),
-currentState, stateQueue,
+currentState, logString="STARTUP LOG";
+/*
+class dataPoint{
+  constructor(){
 
-//Function to call when we recieve data from the CUno
-dataRecieved = function(data){
-    if(data === "BEGIN"){
-        //port.write(Date.now());
-    }
-    else{
-        currentState = JSON.parse(data);
-        stateQueue.push(currentState);
-        if(stateQueue.length >= 200){
-            stateQueue.shift();
-        }
-    }
-};
-
+  }
+}
+*/
 /*
 port.open();
 
 port.on('data', dataRecieved(data)); //When we recieve data, call dataRecieved
 */
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {bManualRotorCurrent: true, fRotorCurrent:0.0, boxStateQueue:[], chartDisplayData:[], sDataToDisplay:"voltage"}
+    this.currentToggled = this.currentToggled.bind(this);
+  }
+  
+  //Function to call when we recieve data from the CUno
+  dataRecieved = function(data){
+    if(data === "BEGIN"){
+        //port.write(Date.now());
+    }
+    else{
+        currentState = JSON.parse(data);
+        this.state.boxStateQueue.push(currentState);
+        if(this.state.boxStateQueue.length >= 200){
+          this.state.boxStateQueue.shift();
+        }
+    }
+
+  };
+
+
+  currentToggled(event){
+    logString = "Event fired! Value: "+ event.target.checked;
+    this.setState({
+      bManualRotorCurrent: event.target.checked
+    })
+    if(this.state.bManualRotorCurrent){
+      //port.write("")
+    }
+    else{
+      //port.write("")
+    }
+  }
+
+  currentFloatChanged(event){
+    this.setState({
+      fRotorCurrent: event.target.value
+    })
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -46,28 +79,33 @@ class App extends Component {
           verticalGrid
           width={1000}
           height={250}
-          data={[
-            [
-              { x: '00:00:01', y: 20 },
-              { x: '00:01:01', y: 10 },
-              { x: '00:01:02', y: 33 },
-              { x: '00:04:03', y: 45 },
-              { x: '00:05:04', y: 15 }
-            ]/*, [
-              { x: '00:00', y: 10 },
-              { x: '00:01', y: 15 },
-              { x: '00:02', y: 13 },
-              { x: '00:04', y: 15 },
-              { x: '00:03', y: 10 }
-            ]*/
-          ]}
+          data={this.state.chartDisplayData}
         />
-        <Checkbox>
-          Automatic rotor current control
-        </Checkbox>
+        <input type="checkbox" onChange={(evt) => this.currentToggled(evt)} checked = {this.state.bManualRotorCurrent}/>Manual rotor current control
+        <input type="number" disabled = {!this.state.bManualRotorCurrent} value = {this.state.fRotorCurrent} onChange={(evt) => this.currentFloatChanged(evt)}/>Amps
+        <select>
+          <option value="voltage">Voltage</option>
+          <option value="rotorcurrent">Rotor current</option>
+        </select>
+        <h1/>{logString}
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+/*
+<LineChart
+          xType={'time'}
+          datePattern={'%H:%M:%S'}
+          axes
+          grid
+          verticalGrid
+          width={1000}
+          height={250}
+          data={[]}
+        />
+*/
