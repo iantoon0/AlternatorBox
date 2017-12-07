07bc38675d71eba2e84891b773f32dd38ea5dc68
iantoon0 <ihunickesmith@gmail.com>
2017-12-07 08:53:22 -0700

    Continued work on server.js

--- ABox_Window/Raspi2Socket/Server.js
+++ ABox_Window/Raspi2Socket/Server.js
@@ -1,33 +1,44 @@
-const io = require('socket.io')(),
+const socket = require('socket.io'),
+http = require('http'),
 fs = require('fs'),
 raspi = require('raspi'),
 Serial = require('raspi-serial').Serial;
 
-var port = new Serial({'baudRate':28800}),
-
+var serialPort = new Serial({'baudRate':28800}),
+server=http.createServer(),
+clientSocket,
+dataToSend,
+io = socket(server);
 
 //Function to call when we recieve data from the CUno
 dataRecieved = function(data){
     if(data === "BEGIN"){
-        port.write(Date.now());
+        serialPort.write(Date.now());
     }
     else{
-        currentState = JSON.parse(data);
-        stateQueue.push(currentState);
-        if(stateQueue.length >= 200){
-            stateQueue.shift();
-        }
+        dataToSend=data;
+        EmitData(clientSocket);
     }
 };
-
+const EmitData = async socket => {
+    try {
+      socket.emit("Data", dataToSend); // Emitting a new message. It will be consumed by the client
+    } catch (error) {
+      console.error(`Error: ${error.code}`);
+    }
+};
 io.on('connection', (client) => {
     // here you can start emitting events to the client 
+    clientSocket = client;
+    client.on("DataChanged",function(){
+
+    });
 });
 const port = 8080;
 io.listen(port);
 console.log('listening on port ', port);
 
 
-port.open();
+serialPort.open();
 
-port.on('data', dataRecieved(data)); //When we recieve data, call dataRecieved
+serialPort.on('data',(data) => dataRecieved()); //When we recieve data, call dataRecieved
